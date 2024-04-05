78a8eb9cc1ec99444da56802614a4d24f46a1273
jo33200 <jo33200@gmail.com>
2024-04-05 10:48:54 +0200

    fix: file home

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Home from './pages/home/';
+import Home from './pages/home';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
