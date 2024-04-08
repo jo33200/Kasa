0bbcdbcec4a0bdffd447a4720279958bced43eca
jo33200 <jo33200@gmail.com>
2024-04-08 14:16:37 +0200

    build: footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './Footer.scss';
+import logo from '../../assets/LOGO-white.svg';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <img src={logo} alt="logo de Kaza" className="logo" />
+            <div>
+                <p>© 2020 Kasa. All rights reserved</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
