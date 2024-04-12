494cd308dd8fe6b194098cd1ef12078e1845d97b
jo33200 <jo33200@gmail.com>
2024-04-12 15:57:05 +0200

    build: tags

---
+++ src/components/Tags/Tags.jsx
@@ -0,0 +1,13 @@
+import './Tags.scss'
+
+function Tags({tags}) {
+    return (
+        <div className='tags'>
+            {tags.map((tag, index) => (
+                <span key={index} className='tag'>{tag}</span>
+            ))}
+        </div>
+    )
+}
+
+export default Tags
