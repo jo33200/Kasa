e5cc49bca430a66ba39f6964a14e5586fe43e200
jo33200 <jo33200@gmail.com>
2024-04-12 16:40:54 +0200

    fix: navBar container

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,11 +5,13 @@ import logo from '../../assets/LOGO.svg';
 
 const NavBar = () => {
     return (
-        <div className="navBar">
-            <img src={logo} alt="logo de Kaza" className="logo" />
-            <div className="links">
-                <Link to="/" className="link">Accueil</Link>
-                <Link to="/about" className="link">A Propos</Link>
+        <div className="navbar-container">
+            <div className="navBar">
+                <img src={logo} alt="logo de Kaza" className="logo" />
+                <div className="links">
+                    <Link to="/" className="link">Accueil</Link>
+                    <Link to="/about" className="link">A Propos</Link>
+                </div>
             </div>
         </div>
     )
