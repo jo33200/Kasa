1bdf716e070d0e064a77422761e936ce7e319851
jo33200 <jo33200@gmail.com>
2024-04-12 10:49:13 +0200

    fix: page error media queries

--- src/pages/error/error.jsx
+++ src/pages/error/error.jsx
@@ -5,7 +5,7 @@ export default function Error() {
     return (
         <div className="error">
             <h1>404</h1>
-            <h2>Oups! La page que vous demandez n'existe pas.</h2>
+            <h2>Oups! La page que<span className="break"><br/></span> vous demandez n'existe pas. </h2>
             <Link to="/">Retourner sur la page d'accueil</Link>
         </div>
     )
