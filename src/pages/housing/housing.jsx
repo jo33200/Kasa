7f665470ef28227e6e2eb5b5cddf10ccaab9f7be
jo33200 <jo33200@gmail.com>
2024-04-12 17:11:17 +0200

    fix: housing id

--- src/pages/housing/housing.jsx
+++ src/pages/housing/housing.jsx
@@ -1,10 +1,35 @@
 import Slideshow from '../../components/Slideshow/Slideshow'
 import './housing.scss';
+import Collapse from '../../components/Collapse/Collapse'
+import InfoAppartement from '../../data/housing.json';
+import Tags from '../../components/Tags/Tags';
+import { useParams } from 'react-router-dom';
 
 function Housing() {
+
+    const { id } = useParams();
+
+    const apartment = InfoAppartement.find((apartment) => apartment.id === id);
+    if (!apartment) {
+        return <div>Appartement non trouvé</div>;
+    }
+
+    const {title, location, description ,equipments, tags} = apartment;
+
     return (
         <div className='housing'>
             <Slideshow />
+            <div className='rental-location'>
+                <h3>{title}</h3>
+                <p>{location}</p> 
+                <div className='rental-tags'>
+                    {tags.map((tag, index) => <Tags key={index} tag={tag} />)}
+                </div>
+                <div className='info-container'>
+                <Collapse choice="Description" text={description} />
+                <Collapse choice="Équipements" text={equipments} />
+                </div>
+            </div> 
         </div>
     );
 }
