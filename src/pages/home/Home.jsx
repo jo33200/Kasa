1f748461e2bd6537beeda4607c91c8473af5181b
jo33200 <jo33200@gmail.com>
2024-04-15 09:40:43 +0200

    fix: home banner text

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,7 +8,11 @@ function Home() {
 
   return (
     <div className='home'>
-      <Banner imgSrc={imgAccueil} altText='vue de falaises' overlay>Chez vous, partout et ailleurs</Banner>
+      <Banner imgSrc={imgAccueil} altText='vue de falaises' overlay>
+            <span className="desktop-text">Chez vous, partout et ailleurs</span>
+            <span className="mobile-text">Chez vous,</span>
+            <span className="mobile-text">partout et ailleurs</span>
+        </Banner>
       <CardContainer />
     </div>
   );
