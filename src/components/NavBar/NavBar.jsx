1b3d4413ca5fa5835e8075c3c1c38fbbe1b59d7f
jo33200 <jo33200@gmail.com>
2024-05-03 15:52:20 +0200

    fix: link active underligne

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,16 +1,18 @@
 import React from "react"; 
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import './NavBar.scss';
 import logo from '../../assets/LOGO.svg';
 
 const NavBar = () => {
+    const location = useLocation();
+
     return (
         <div className="navbar-container">
             <div className="navBar">
                 <img src={logo} alt="logo de Kaza" className="logo" />
                 <div className="links">
-                    <Link to="/" className="link">Accueil</Link>
-                    <Link to="/about" className="link">A Propos</Link>
+                    <Link to="/" className={location.pathname === "/" ? "link active" : "link"}>Accueil</Link>
+                    <Link to="/about" className={location.pathname === "/about" ? "link active" : "link"}>A Propos</Link>
                 </div>
             </div>
         </div>
